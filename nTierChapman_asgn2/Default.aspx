a2ed2ea8f06b21b5af3c6e7cefd37bda2475d3d4
jonwchapman <jon.w.chapman@gmail.com>
2014-03-18 13:13:10 -0500

    adding contents of asgn 2

---
+++ nTierChapman_asgn2/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="wellformedsqlchecker" runat="server">
+    <div style="height: 382px">
+    
+        <br />
+        <br />
+        <br />
+    
+        <asp:TextBox ID="TextBox1" runat="server" Width="475px" MaxLength="150"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnCheckSyntax" runat="server" OnClick="btnCheckSyntax_Click" Text="Validate" />
+    
+        <br />
+        <asp:Label ID="Scrubbed1" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="Errors1" runat="server" Text=""></asp:Label>
+    
+        <br />
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AutoGenerateColumns="true">
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        </asp:GridView>
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ nTierChapman_asgn2/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using myUtilities;
+public partial class _Default : System.Web.UI.Page
+{
+   // public int SQLcounter = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    { 
+    }
+
+    protected void btnCheckSyntax_Click(object sender, EventArgs e)
+    {
+        Errors1.Text = "No Errors Found.";
+        string inputStr = TextBox1.Text;                                                            // set the input string equal to the input in Textbox1
+
+        if (inputStr.Length == 0)
+        {
+            Errors1.Text = "No SQL provided.";                                                      // if the user enters no input, then output to error box, letting user know.
+        }
+        else
+        {
+            inputStr = ScrubSQLstring(inputStr);                                                    // pass inputStr to Scrub function, set to inputStr
+            char[] delimiters = new char[] {' '};                                                   // set delimiter characters
+                                                                                                    // pass scrubbed input, and element count to Build Token array, located in the utilities.cs file.
+            myUtilities.SQLutilities checker = new myUtilities.SQLutilities();                      // create instance of our SQLutilities class
+            string[] tokenArray = checker.BuildTokenArray(inputStr);                                // pass our instance the input string IOT have it build an array consisting of the components(tokens) of the SQL input.
+            checker.CheckSQLsyntax(tokenArray);                                                     // pass the array of tokens to the CheckSQLsyntax() method
+            if (checker.errorFound){                                                                // check to see if errorFound was tripped in our instances.
+                Errors1.Text = checker.getError();                                                  // if they were, then set errors to text box <<<<<<<<<<<<<<<<<<<<<<DEBUGGING STEP>>>>>>>>>>>>>>>>>>>
+            }
+            List<string> lst = tokenArray.OfType<string>().ToList();                                // take the array that we got back and turn it into a list
+
+            GridView1.DataSource = lst;                                                             // set the datasource
+            GridView1.DataBind();                                                                   // bind it
+        }
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+    protected string ScrubSQLstring(string inputStr)
+    {
+        string scrubbedInput = "";                                                                  // Declare variable for the scrubbed input I am creating.
+        while (inputStr.Contains("  ")) {                                                           // While our input string contains more than one space...
+            inputStr = inputStr.Replace("  ", " ");                                                 // then replace those two spaces with one space....repeat as necessary
+        }
+        while (inputStr.Contains(" ,")) {                                                           // While our input string contains a space before a comma
+            inputStr = inputStr.Replace(" ,", ",");                                                 // replace with just a comma.
+        }
+        while (inputStr.Contains(" ;")) {                                                           // While our input string contains a space before a semicolon
+            inputStr = inputStr.Replace(" ;", ";");                                                 // replace with just a semicolon.
+        }
+        scrubbedInput = inputStr.Trim();                                                            // Trim leaing and trailing spaces from input and save it to scrubbedInput.
+        return scrubbedInput;
+    }
+}     
+
+
+//Scrubbed1.Text = scrubbedInput;  // OUTPUT to scrubbed box on page for debugging
+//int count = inputStr.Split(' ').Length;     
+//string[] strarray = myUtilities.SQLutilities.BuildTokenArray(inputStr, count);
